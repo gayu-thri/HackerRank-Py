20e61d1288969e002a9ce9988cf7dcff9fc9c621
gayu-thri <egayu3@gmail.com>
2020-02-12 09:41:13 +0530

    Create Word Order.py
    Counts distinct words from the input and counts word frequencies using a dictionary.

---
+++ Word Order/Word Order.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+l = list()
+out = []
+count = 0 
+d = dict()
+for i in range(n):
+    l.append(input())
+for i in range(n):
+    if(l[i] in d.keys()):
+        d[l[i]] += 1
+    else:
+        d[l[i]] = 1
+for key,val in d.items():
+    count += 1
+    out.append(val)
+print(count)
+for ch in out:
+    print(ch,end=" ")
